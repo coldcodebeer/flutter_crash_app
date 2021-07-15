3fd0031d52a78c5c9b3acf6096505ad6b8d07109
Spencer <happy_programming@aliyun.com>
2021-07-15 14:38:05 +0800

    feat: Images & Assets

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,8 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'hello world',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower'
-          ),
+        child: Image(
+          image: AssetImage('assets/dog2.jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
