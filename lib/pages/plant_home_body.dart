a28950b94e23674d2411ff955c961697ebaeb69a
Spencer <happy_programming@protonmail.com>
2021-07-18 12:24:30 +0800

    feat: plant title

--- lib/pages/plant_home_body.dart
+++ lib/pages/plant_home_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/plant_header_with_search_box.dart';
+import 'package:flutter_app/pages/plant_title.dart';
+import 'package:flutter_app/utilities/constants.dart';
 
 class PlantHomeBody extends StatelessWidget {
   const PlantHomeBody({Key? key}) : super(key: key);
@@ -11,9 +13,12 @@ class PlantHomeBody extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
+          TitleWithMoreButton(
+            title: 'More',
+            press: () {},
+          ),
         ],
       ),
     );
   }
 }
-
